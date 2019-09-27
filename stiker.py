c35e4f7577330e99d68945488ee72bd7c352a470
Gleb <seleznev.gs@phystech.edu>
2019-09-27 16:35:03 +0300

    Stiker

---
+++ stiker.py
@@ -0,0 +1,19 @@
+from graph import *
+
+penColor("black")
+penSize(1)
+brushColor("yellow")
+circle(200,200,95)
+brushColor("black")
+rectangle(150,240,250,220)
+brushColor("red")
+circle(170,170,20)
+circle(230,170,20)
+brushColor("black")
+circle(170,170,10)
+circle(230,170,10)
+penSize(15)
+line(140,125,190,135)
+line(210,135,260,125)
+
+run()
