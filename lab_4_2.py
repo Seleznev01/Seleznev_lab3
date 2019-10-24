d047beaa0dad5e7a48afd57f39a32270dfa339a8
Gleb <seleznev.gs@phystech.edu>
2019-10-24 22:38:19 +0300

    Create Balls with classes

--- lab_4_2.py
+++ lab_4_2.py
@@ -4,74 +4,67 @@ import time
 root = Tk()
 root.geometry('800x600')
 
-canv = Canvas(root,bg='white')
-canv.pack(fill=BOTH,expand=1)
+canv = Canvas(root,bg = 'white')
+canv.pack(fill = BOTH,expand = 1)
 
 colors = ['red','orange','yellow','green','blue']
-#def new_square():
-	#global x3, y3, r3, ball3, dx3, dy3
-	#canv.delete('square1')
-	#y1 = rnd(70, 550)
-	#r1 = rnd(30, 60)
-def new_ball1():
+score = 0
+Balls = []
 
-	global x1, y1, r1, ball1, dx1, dy1, broker
-	canv.delete('ball1')
-	x1 = rnd(70, 400)
-	y1 = rnd(70, 550)
-	r1 = rnd(30, 60)
-	ball1 = canv.create_oval(x1 - r1, y1 - r1, x1 + r1, y1 + r1, fill = choice(colors), width=2, tag = 'ball1')
-	dx1 = rnd(-3, 3)
-	dy1 = rnd(-3, 3)
-	def move1():
-		global x1, y1, dx1, dy1
-		canv.move(ball1, dx1, dy1)
-		x1 = x1 + dx1
-		y1 = y1 + dy1
-		root.after(50, move1)
-		if (x1 <= 0) or (x1 >= 800):
-			dx1 = - dx1
-		if (y1 <= 0) or (y1 >= 600):
-			dy1 = - dy1
-	move1()
-def new_ball2():
-	global x2, y2, r2, ball2, dx2, dy2
-	canv.delete('ball2')
-	x2 = rnd(70, 700)
-	y2 = rnd(70, 550)
-	r2 = rnd(30, 60)
-	ball2 = canv.create_oval(x2 - r2, y2 - r2, x2 + r2, y2 + r2, fill=choice(colors), width=2, tag='ball2')
-	dx2 = 2
-	dy2 = -2
-	def move2():
-		global x2, y2, dx2, dy2
-		canv.move(ball2, dx2, dy2)
-		x2 = x2 + dx2
-		y2 = y2 + dy2
-		root.after(50, move2)
-		if (x2 <= 0) or (x2 >= 800):
-			dx2 = - dx2
-		if (y2 <= 0) or (y2 >= 600):
-			dy2 = - dy2
-	move2()
-count=0
+
+class Ball:
+
+        def __init__(self):
+                self.x = rnd(100, 700)
+                self.y = rnd(100, 500)
+                self.r = rnd(30, 50)
+                self.dx = rnd(-2, 2)
+                self.dy = rnd(-2, 2)
+                self.obj = canv.create_oval(self.x - self.r, self.y - self.r, self.x + self.r,
+                                            self.y + self.r, fill = choice(colors), width = 2, tag = 'ball')
+        def Refr(self):
+                if self.x <= self.r or self.x >= 800 - self.r:
+                        self.dx = - self.dx
+                if self.y <= self.r or self.y >= 600 - self.r:
+                        self.dy = - self.dy
+
+        def Move(self):
+                self.x = self.x + self.dx
+                self.y = self.y + self.dy
+                canv.move(self.obj, self.dx, self.dy)
+        def Delete(self):
+                canv.delete(self.obj)
+
+
+        
+def Create():
+        global Numb
+        for i in range(Numb):
+                Balls.append(Ball())
+                        
+def Play():
+        for i in Balls:
+                i.Move()
+                i.Refr()
+        root.after(20, Play)
+
+def Start():
+
+        global Numb,count
+        Numb = rnd(1,10)
+        canv.delete('ball')
+        Create()
+        Play()
+
 def click(event):
-	global count, x1, y1, x2, y2, broker1
-	if ((event.x - x1) ** 2 + (event.y - y1) ** 2) ** 0.5 <= r1:
-		canv.delete('ball1')
-		canv.delete('count')
-		canv.create_text(750, 50, text='БРС='+str(count + 1), justify = CENTER, font = "Verdana 14", tag = 'count')
-		count = count + 1
-		new_ball1()
-	if ((event.x - x2) ** 2 + (event.y - y2) ** 2) ** 0.5 <= r2:
-		canv.delete('ball2')
-		canv.delete('count')
-		canv.create_text(750, 50, text='БРС='+str(count + 1), justify = CENTER, font = "Verdana 14", tag = 'count')
-		count = count + 1
-		new_ball2()
-
+        global Numb, score, count
+        for i in Balls:
+                if ((event.x - i.x) ** 2 + (event.y - i.y) ** 2) ** 0.5 <= i.r:
+                        score += 1
+                        canv.delete('score')
+                        canv.create_text(750, 50, text = str(score), justify = CENTER, font = "Verdana 14", tag = 'score')
+                        i.Delete()
+                        Balls.append(Ball())
 canv.bind('<Button-1>', click)
-
-new_ball1()
-new_ball2()
+Start()
 mainloop()
